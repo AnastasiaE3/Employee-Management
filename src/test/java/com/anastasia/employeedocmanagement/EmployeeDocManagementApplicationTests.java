36120305b48a689db5499e9d43f949ba567633ec
AnastasiaE3 <epiart2000@gmail.com>
2025-03-10 19:16:35 -0400

    commit

---
+++ src/test/java/com/anastasia/employeedocmanagement/EmployeeDocManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anastasia.employeedocmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDocManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
